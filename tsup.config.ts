b9254b782b7f6715590f6dfbbdf6c2998a8e20df
JustEvil <thatjustevil@gmail.com>
2024-09-20 15:06:55 -0600

    deleted useless comments

--- tsup.config.ts
+++ tsup.config.ts
@@ -10,18 +10,3 @@ export default defineConfig({
 	skipNodeModulesBundle: true,
 	ignoreWatch: ["**/node_modules/**", "**/.git/**"],
 });
-
-//defineConfig({
-//	entry: ["./src/**/*.ts"],
-//	format: ["esm", "cjs"],
-//	clean: true,
-//	dts: true,
-//	minify: true,
-//	target: "es2022",
-//	bundle: true,
-//	splitting: false,
-//	sourcemap: true,
-//	keepNames: true,
-//	skipNodeModulesBundle: true,
-//	ignoreWatch: ["**/node_modules/**", "**/.git/**"],
-//});
