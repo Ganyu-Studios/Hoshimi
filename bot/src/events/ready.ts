e778f57629e02c17a239153b8056567a542af25a
JustEvil <thatjustevil@gmail.com>
2024-10-16 20:05:18 -0600

    cleaned the testbot

--- bot/src/events/ready.ts
+++
@@ -1,9 +0,0 @@
-import { createEvent } from "seyfert";
-
-export default createEvent({
-	data: { name: "ready", once: true },
-	run: async (user, client) => {
-		await client.uploadCommands({ cachePath: "commands.json" });
-		client.logger.info(`Logged in as: ${user.username}`);
-	},
-});
--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,13 +1,9 @@
 process.loadEnvFile();
 
 import { Client, type User, type ParseClient } from "seyfert";
-import { Connectors, type NodeOption } from "shoukaku";
 import { Yuna } from "yunaforseyfert";
 
-import { autoplayFn } from "./autoplay";
-
 import { HandleCommand } from "seyfert/lib/commands/handle.js";
-import { Manager, SearchEngines } from "hoshimi";
 
 const nodes: NodeOption[] = [
 	{
@@ -19,8 +15,6 @@ const nodes: NodeOption[] = [
 const prefixes: string[] = ["arle", "arlecchino"];
 
 class TestClient extends Client<true> {
-	readonly manager: Manager;
-
 	constructor() {
 		super({
 			allowedMentions: {
@@ -34,25 +28,6 @@ class TestClient extends Client<true> {
 				}),
 			},
 		});
-
-		this.manager = new Manager(
-			new Connectors.Seyfert(this),
-			{
-				nodes,
-				sendPayload: (guildId, payload) =>
-					this.gateway.send(this.gateway.calculateShardId(guildId), payload),
-				maxPreviousTracks: 10,
-				defaultSearchEngine: SearchEngines.Youtube,
-				autoplayFn,
-			},
-			{
-				reconnectTries: 5,
-				resume: true,
-				resumeByLibrary: true,
-				resumeTimeout: 60000,
-			},
-		);
-
 		this.run();
 	}
 
@@ -68,51 +43,6 @@ class TestClient extends Client<true> {
 }
 
 const client = new TestClient();
-
-//
-// Node Events
-//
-client.manager.shoukaku.on("ready", (name) =>
-	client.logger.info(`Lavalink node: ${name} connected.`),
-);
-client.manager.shoukaku.on("error", (name, error) =>
-	client.logger.info(`Lavalink node: ${name} has an error: ${error}.`),
-);
-client.manager.shoukaku.on("disconnect", (name, retries) =>
-	client.logger.info(`Lavalink node: ${name} is disconnected: Retries: ${retries}.`),
-);
-client.manager.shoukaku.on("reconnecting", (name, left) =>
-	client.logger.info(`Lavalink node: ${name} is reconnecting: Left: ${left}.`),
-);
-
-//
-// Manager Events
-//
-client.manager.on("trackStart", (player, track) => {
-	if (!track) return;
-
-	const requester = track.requester as User;
-
-	console.info(client.manager.options.storage?.get(player.guildId));
-
-	client.messages.write(player.textId!, {
-		content: `Now playing: ${track.toHyperlink()}\nRequested by: ${requester.tag}`,
-	});
-});
-client.manager.on("trackEnd", (player, track) => {
-	if (!track) return;
-
-	client.messages.write(player.textId!, {
-		content: `Song ended: ${track.info.title}`,
-	});
-});
-client.manager.on("queueEnd", (player) => {
-	client.messages.write(player.textId!, {
-		content: "Queue ended",
-	});
-});
-
-client.manager.on("debug", (message) => client.logger.info(message));
 
 declare module "seyfert" {
 	interface UsingClient extends ParseClient<TestClient> {}
