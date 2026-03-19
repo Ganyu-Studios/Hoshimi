d4a51d3bdc6dfc6f864d8b0a96b5c9b495550a03
JustEvil <71156616+EvilG-MC@users.noreply.github.com>
2026-03-19 06:40:37 -0600

    change nodelink types

--- bot/src/nodelink/types.ts
+++ bot/src/nodelink/types.ts
@@ -428,9 +428,9 @@ export interface PauseEvent extends PlayerEvent<NodelinkEventType.Pause> {
 export interface ConnectionStatusEvent extends PlayerEvent<NodelinkEventType.ConnectionStatus> {
     /**
      * The connection metrics.
-     * @type {NodelinkConnection}
+     * @type {NodelinkConnectionMetrics}
      */
-    metrics: NodelinkConnection;
+    metrics: NodelinkConnectionMetrics;
 }
 
 /**
