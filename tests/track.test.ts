5d13a5d8563c5a336054f875208ee89c9c0a41c8
JustEvil <71156616+EvilG-MC@users.noreply.github.com>
2026-07-12 10:15:51 -0600

    refactor(tests): consolidate imports in track test file

--- tests/track.test.ts
+++ tests/track.test.ts
@@ -1,9 +1,7 @@
 import { describe, expect, it, vi } from "vitest";
-
 import { ResolveError } from "../src/classes/Errors";
 import { Track, UnresolvedTrack } from "../src/classes/Track";
-import { LoadType, SourceNames } from "../src/types/Node";
-import { State } from "../src/types/Node";
+import { LoadType, SourceNames, State } from "../src/types/Node";
 import { createMockTrackData, createRealManager, createRealNode, createRealPlayer } from "./helpers";
 
 describe("Track", () => {
