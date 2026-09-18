524af4a2aeaccbe23efe578925d37505015954eb
UnknownDxv <piyushxyz143@gmail.com>
2026-09-18 20:41:27 +0530

    feat(player): add isPaused method

--- src/classes/player/Player.ts
+++ src/classes/player/Player.ts
@@ -262,6 +262,14 @@ export class Player {
     }
 
     /**
+     * Check if the player is currently paused.
+     * @returns {boolean} Whether the player is currently paused.
+     */
+    public isPaused(): boolean {
+        return !this.playing && this.paused;
+    }
+
+    /**
      * Check if the player is currently idle.
      * @returns {boolean} Whether the player is currently idle.
      */
